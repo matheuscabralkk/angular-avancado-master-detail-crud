77f37cfd994e16fd7c9b62a911fe1c8efdcc75e0
Matheus <skt.matheus@hotmail.com>
2019-04-01 14:53:57 -0300

    adding category service

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -22,19 +22,52 @@ private apiPath: string = "api/categories";
     )
   }
 
+  getById(id:number): Observable<Category> {
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  create (category : Category) : Observable<Category> {
+    return this.http.post(this.apiPath,category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  update (category : Category) : Observable<Category> {
+    const url = `${this.apiPath}/${category.id}`;
+
+    return this.http.put(url,category).pipe(
+      catchError(this.handleError),
+      map(() => category)
+    )
+  }
+
+  delete (id : number) : Observable<any> {
+    const url = `${this.apiPath}/${id}`;
+    return this.http.delete(url).pipe(
+      catchError(this.handleError),
+      map(() => null)
+    )
+  }
+
+
 
   //private methods
-
   private jsonDataToCategories(jsonData: any[]): Category[]{
     const categories: Category[] = [];
     jsonData.forEach(element => categories.push(element as Category));
     return categories;
   }
-
+  private jsonDataToCategory(jsonData : any): Category {
+    return jsonData as Category;
+  }
   private handleError(error:any):Observable<any>{
-
     console.log("Erro na Requisição => ", error)
-
     return throwError(error)
   }
 }
